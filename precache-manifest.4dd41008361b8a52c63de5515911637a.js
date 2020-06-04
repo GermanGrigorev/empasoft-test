b44ede9e12524eaa662f8505a7764c29408c817b
Grigorev German <ggrigoriev.000spb@gmail.com>
2020-06-04 23:44:07 +0300

    Updates

---
+++ precache-manifest.4dd41008361b8a52c63de5515911637a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ad79917ea20a2e03017a941244811a",
+    "url": "/empasoft-test/index.html"
+  },
+  {
+    "revision": "e58798ef5fb2fe6848e1",
+    "url": "/empasoft-test/static/css/main.f3c63dc9.chunk.css"
+  },
+  {
+    "revision": "92a91b7fc115e9ad00c8",
+    "url": "/empasoft-test/static/js/2.55dff576.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/empasoft-test/static/js/2.55dff576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58798ef5fb2fe6848e1",
+    "url": "/empasoft-test/static/js/main.76eb768e.chunk.js"
+  },
+  {
+    "revision": "5f7c7dc32a2ce2c883db",
+    "url": "/empasoft-test/static/js/runtime-main.66b09b9a.js"
+  }
+]);
